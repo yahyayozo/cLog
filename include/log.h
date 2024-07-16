dc5b2fa467198762cce38ad2f835d975d7eff14a
yaouledamer <yahya.aouled@habemus.com>
2024-07-16 11:34:22 +0100

    Added some other APIs

--- include/log.h
+++ include/log.h
@@ -5,7 +5,18 @@
 #include "stdint.h"
 #define ENABLE_LOGGING
 
+#define IS_LOG_LEVEL(x) (x > LOG_LEVEL_INFO && x < LOG_LEVEL_CRITICAL)
+
 typedef void (*Log_PrintFunc)(char *data, uint32_t dataLength);
+
+typedef enum
+{
+    LOG_STATUS_OK,
+    LOG_STATUS_NOT_INITIALIZED,
+    LOG_STATUS_DISABLED,
+    LOG_STATUS_INCORRECT_LEVEL,
+    LOG_STATUS_LEVEL_UNDER_THRESHOLD,
+} Log_Status;
 
 typedef enum
 {
@@ -16,17 +27,23 @@ typedef enum
 } Log_Level;
 
 #ifdef ENABLE_LOGGING
-void LOG_INIT(Log_PrintFunc printCallback, Log_Level logLevelThreshold);
-void LOG_PRINT(Log_Level logLevelThreshold, char *format, ...);
-void LOG_SET_LEVEL_THRESHOLD(Log_Level LogLevelThreshold);
-void LOG_SET_PRINT_CALLBACK(Log_PrintFunc printCallback);
+Log_Status LOG_INIT(Log_PrintFunc printCallback, Log_Level logLevelThreshold);
+Log_Status LOG_PRINT(Log_Level logLevelThreshold, char *format, ...);
+Log_Status LOG_SET_LEVEL_THRESHOLD(Log_Level LogLevelThreshold);
+Log_Level LOG_GET_LEVEL_THRESHOLD();
+Log_Status LOG_SET_PRINT_CALLBACK(Log_PrintFunc printCallback);
 char *LOG_LEVEL_TO_STRING(Log_Level logLevel);
+Log_Status LOG_ENABLE();
+Log_Status LOG_DISABLE();
 #else
-#define LOG_INIT(a,b)
-#define LOG_PRINT(a,b)  do{} while(0)
-#define LOG_SET_LEVEL_THRESHOLD(a)
-#define LOG_SET_PRINT_CALLBACK(a)
-#define LOG_LEVEL_TO_STRING(a)
+#define LOG_INIT(a, b) LOG_STATUS_DISABLED
+#define LOG_PRINT(a, b) LOG_STATUS_DISABLED
+#define LOG_SET_LEVEL_THRESHOLD(a) LOG_STATUS_DISABLED
+#define LOG_GET_LEVEL_THRESHOLD() LOG_STATUS_DISABLED
+#define LOG_SET_PRINT_CALLBACK(a) LOG_STATUS_DISABLED
+#define LOG_LEVEL_TO_STRING(a) LOG_STATUS_DISABLED
+#define LOG_ENABLE() LOG_STATUS_DISABLED
+#define LOG_DISABLE() LOG_STATUS_DISABLED
 #endif
 
 #endif
--- src/log.c
+++ src/log.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "string.h"
+#include "stdbool.h"
 
 #ifdef ENABLE_LOGGING
 
@@ -10,22 +11,42 @@ struct
 {
     Log_PrintFunc LogPrintCallback;
     Log_Level LogLevelThreshold;
+    bool IsLogEnabled;
+    bool IsInitialized;
 } Log_ConfigData;
 
-void LOG_INIT(Log_PrintFunc printCallback, Log_Level logLevelThreshold)
+Log_Status LOG_INIT(Log_PrintFunc printCallback, Log_Level logLevelThreshold)
 {
+    if (!IS_LOG_LEVEL(logLevelThreshold))
+        return LOG_STATUS_INCORRECT_LEVEL;
 
+    memset(&Log_ConfigData, 0, sizeof(Log_ConfigData));
     Log_ConfigData.LogPrintCallback = printCallback;
+    Log_ConfigData.IsLogEnabled = true;
+    Log_ConfigData.IsInitialized = true;
     Log_ConfigData.LogLevelThreshold = logLevelThreshold;
+
+    return LOG_STATUS_OK;
 }
 
-void LOG_PRINT(Log_Level logLevel, char *format, ...)
+Log_Status LOG_PRINT(Log_Level logLevel, char *format, ...)
 {
+
+    if (!Log_ConfigData.IsInitialized)
+        return LOG_STATUS_NOT_INITIALIZED;
+
+    if (!Log_ConfigData.IsLogEnabled)
+        return LOG_STATUS_DISABLED;
+
+    if (!IS_LOG_LEVEL(logLevel))
+        return LOG_STATUS_INCORRECT_LEVEL;
+
+    if (logLevel < Log_ConfigData.LogLevelThreshold)
+        return LOG_STATUS_LEVEL_UNDER_THRESHOLD;
+
     static char printMsg[50];
     memset(printMsg, 0, sizeof(printMsg));
 
-    if (logLevel < Log_ConfigData.LogLevelThreshold)
-        return;
     uint8_t writeIndex = snprintf(printMsg, sizeof(printMsg), "[ %s ] ", LOG_LEVEL_TO_STRING(logLevel));
     va_list args;
     va_start(args, format);
@@ -33,16 +54,27 @@ void LOG_PRINT(Log_Level logLevel, char *format, ...)
     va_end(args);
 
     Log_ConfigData.LogPrintCallback(printMsg, strlen(printMsg));
+
+    return LOG_STATUS_OK;
 }
 
-void LOG_SET_LEVEL_THRESHOLD(Log_Level LogLevelThreshold)
+Log_Status LOG_SET_LEVEL_THRESHOLD(Log_Level LogLevelThreshold)
 {
+    if (!Log_ConfigData.IsInitialized)
+        return LOG_STATUS_NOT_INITIALIZED;
+
     Log_ConfigData.LogLevelThreshold = LogLevelThreshold;
 }
 
-void LOG_SET_PRINT_CALLBACK(Log_PrintFunc printCallback)
+Log_Level LOG_GET_LEVEL_THRESHOLD()
+{
+    return Log_ConfigData.LogLevelThreshold;
+}
+
+Log_Status LOG_SET_PRINT_CALLBACK(Log_PrintFunc printCallback)
 {
     Log_ConfigData.LogPrintCallback = printCallback;
+    return LOG_STATUS_OK;
 }
 
 char *LOG_LEVEL_TO_STRING(Log_Level logLevel)
@@ -60,6 +92,24 @@ char *LOG_LEVEL_TO_STRING(Log_Level logLevel)
     default:
         break;
     }
+}
+
+Log_Status LOG_ENABLE()
+{
+    if (!Log_ConfigData.IsInitialized)
+        return LOG_STATUS_NOT_INITIALIZED;
+
+    Log_ConfigData.IsLogEnabled = true;
+    return LOG_STATUS_OK;
+}
+
+Log_Status LOG_DISABLE()
+{
+    if (!Log_ConfigData.IsInitialized)
+        return LOG_STATUS_NOT_INITIALIZED;
+
+    Log_ConfigData.IsLogEnabled = false;
+    return LOG_STATUS_OK;
 }
 
 #endif /* ENABLE_LOGGING */
